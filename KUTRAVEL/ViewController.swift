8608c0504b6b70ff72c54b609c17cdfa1e40f089
Begum Sen <begumsen19@ku.edu.tr>
2023-05-23 20:14:48 +0300

    Initial Commit

---
+++ KUTRAVEL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KUTRAVEL
+//
+//  Created by Begum Sen on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
